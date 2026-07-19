c7f124db85e669b11994e53490a26beb9e57dac9
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2026-07-19 15:54:49 -0700

    Import

---
+++ build.sh
@@ -0,0 +1,10 @@
+mkapk.py \
+    -n fixptxp \
+    -v 0.0.1 \
+    -d src \
+    -a noarch \
+    -D 'Fix PtxP' \
+    -u 'https://github.com/kn6plv/fixptxp' \
+    -l 'MIT' \
+    -m 'tim.j.wilkinson@gmail.com' \
+    -o .
